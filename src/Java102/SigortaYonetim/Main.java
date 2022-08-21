8e273f81f679208ef26585f90557946d8cccca30
csmmbpro <toprakcelaleddin@gmail.com>
2022-08-21 16:16:47 +0300

    Java 102 - Patika.dev Ödev - Sigorta Yönetim Sistemi

---
+++ src/Java102/SigortaYonetim/Main.java
@@ -0,0 +1,15 @@
+package Java102.SigortaYonetim;
+
+public class Main {
+    public static void main(String[] args) {
+        AccountManager accountManager = new AccountManager();
+        Account account = accountManager.login();
+        System.out.println("Hoşgeldiniz " + account.getUser().getFname() + " " +
+                account.getUser().getLname());
+        account.addAddress("İnönü / Eskişehir");
+        account.addAddress("Ayvacık / Çanakkale");
+        account.addAddress("Çerkezköy / Tekirdağ");
+        account.addInsurance(new CarInsurance());
+        account.showUserInfo();
+    }
+}
